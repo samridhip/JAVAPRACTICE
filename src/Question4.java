de5b4ac669cf234641c8bfbd3ea23ee10df87753
samridhip <pandeysamridhi1@gmail.com>
2023-11-27 05:12:04 +0530

    practice

---
+++ src/Question4.java
@@ -0,0 +1,12 @@
+public class Question4 {
+    public static void main(String[]args){
+        int first= -5+8*6;
+        int second= (55+9)%9;
+        int third= 20+-3*5/8;
+        int fourth= 5+15/3*2-8%3;
+        System.out.println(first);
+        System.out.println(second);
+        System.out.println(third);
+        System.out.println(fourth);
+    }
+}
