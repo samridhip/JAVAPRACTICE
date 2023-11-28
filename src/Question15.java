85926ac9fe235a51470f4e418300e6bb00a0e7cd
samridhip <pandeysamridhi1@gmail.com>
2023-11-29 03:35:25 +0530

    practice

---
+++ src/Question15.java
@@ -0,0 +1,14 @@
+public class Question15 {
+    public static void main(String[]args){
+        int a,b,temp;
+        a=4;
+         b=5;
+        System.out.println("before swapping:"+a+b);
+         temp= a;
+
+        a = b;
+         b= temp;
+        System.out.println("after swapping:"+a+b);
+
+    }
+}
---
+++ src/Question16.java
@@ -0,0 +1,5 @@
+public class Question16 {
+    public static void main(String[]args){
+        System.out.println();
+    }
+}
