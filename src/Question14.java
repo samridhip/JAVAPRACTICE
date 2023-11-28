89a1b823dc2fb5496be6a0e6ca333985b22fa6ec
samridhip <pandeysamridhi1@gmail.com>
2023-11-29 01:58:57 +0530

    practice

---
+++ src/Question14.java
@@ -0,0 +1,8 @@
+public class Question14 {
+    public static void main(String[]args){
+       System.out.println("* * * * * * =================================");
+       System.out.println("* * * * *  ================================== ");
+       System.out.println("* * * * * * ==================================");
+      System.out.println("* * * * *  ==================================");
+      }
+}
