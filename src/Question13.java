812d0cb6c6bf28106c1fff38c24f9c81a195e911
samridhip <pandeysamridhi1@gmail.com>
2023-11-29 01:45:23 +0530

    practice

---
+++ src/Question13.java
@@ -0,0 +1,8 @@
+public class Question13 {
+    public static void main(String[]args){
+        double V = 5.5;
+        double T= 8.5;
+        System.out.println("Area is :"+(V*T));
+        System.out.println("Perimeter is:"+(2*(V+T)));
+    }
+}
