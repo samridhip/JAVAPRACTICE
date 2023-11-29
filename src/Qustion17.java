b5a5f94bca2715bee431b846f7044fc5344b0345
samridhip <pandeysamridhi1@gmail.com>
2023-11-30 01:21:31 +0530

    practice

---
+++ src/Qustion17.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Qustion17 {
+    public static void main(String[]args){
+        Scanner name= new Scanner(System.in);
+        System.out.println("Input first binary number:");
+        int input= name.nextInt();
+        System.out.println("Input second binary number:");
+        int output=name.nextInt();
+        System.out.println("")
+    }
+}
